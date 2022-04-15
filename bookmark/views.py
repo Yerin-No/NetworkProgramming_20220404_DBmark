eba9be3d7861cc6943a8be067637103579d03535
Yerin-No <w2006@e-mirim.hs.kr>
2022-04-15 13:54:22 +0900

    views BookmarkCreateView

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from bookmark.models import Bookmark
 
 class BookmarkListview(ListView):
     model = Bookmark
     #bookmark_list.html, {'bookmark_list':Bookmark.object.all()}
 
-
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['name', 'url']    # '__all__'
+    template_name_suffix = '_create'    # bookmark_form.html -> bookmark_create.html
+    success_url = reverse_lazy('bookmark:list')
