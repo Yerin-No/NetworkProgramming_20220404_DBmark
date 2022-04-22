9699cc4bf363699a7466e1660008e4fd4f012bf8
Yerin-No <w2006@e-mirim.hs.kr>
2022-04-22 13:50:40 +0900

    views BookmarkUpdateView

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from bookmark.models import Bookmark
 
 class BookmarkListview(ListView):
     model = Bookmark
-    #bookmark_list.html, {'bookmark_list':Bookmark.object.all()}
+    # bookmark_list.html, {'bookmark_list':Bookmark.object.all()}
 
 class BookmarkCreateView(CreateView):
     model = Bookmark
@@ -15,3 +15,10 @@ class BookmarkCreateView(CreateView):
 
 class BookmarkDetailView(DetailView):
     model = Bookmark
+
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    fields = ['name', 'url']    # '__all__'
+    template_name_suffix = '_update'    # bookmark_update.html
+    success_url = reverse_lazy('bookmark:list')
