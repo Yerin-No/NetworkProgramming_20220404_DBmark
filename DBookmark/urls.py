dd13286b0140a36d4babd1d303326c8258545800
Yerin-No <w2006@e-mirim.hs.kr>
2022-04-25 14:58:11 +0900

    -> bookmark/list home

--- DBookmark/urls.py
+++ DBookmark/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from bookmark.views import BookmarkListview
+
 urlpatterns = [
+    path('', BookmarkListview.as_view()),
     path('bookmark/', include('bookmark.urls')),
     path('admin/', admin.site.urls),
 ]
